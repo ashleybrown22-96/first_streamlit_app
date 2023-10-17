c50119ccf830ed9c9b3bcc231d5dc822d229cac6
ashleybrown22-96 <147751181+ashleybrown22-96@users.noreply.github.com>
2023-10-17 12:18:52 -0600

    Update streamlit_app.py
    Added code using pandas and display as a table

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,6 +24,12 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
+#writes the data to the screen
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
 
+# uses pandas to read the data
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+
+# writes data to a table 
+streamlit.dataframe(fruityvice_normalized)
